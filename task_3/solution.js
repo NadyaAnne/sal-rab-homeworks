49c2d06f9631f67b20091f07215c044ea548061a
NadyaAnne <naday.anne.1995@gmail.com>
2022-11-21 00:05:27 +0300

    4commit

--- task_3/solution.js
+++ task_3/solution.js
@@ -24,12 +24,12 @@ function renderCartItem(item) {
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 let itemCountText = '';
 itemCountText = itemCountText + product.count;
-itemCountText = itemCountText + `x`;
+itemCountText = itemCountText + ` x `;
 itemCountText = itemCountText + product.price;
-itemCountText = itemCountText + `₽ =`;
-let sum = count * price;
+itemCountText = itemCountText + ` ₽ = `;
+let sum = product.count * product.price;
 itemCountText = itemCountText + sum;
-itemCountText = itemCountText + `₽`;
+itemCountText = itemCountText + ` ₽`;
 
     // Конец решения задания №3.1.
 
