ac41c751b03ca9e2695cd87eb882182faff27bf3
NadyaAnne <naday.anne.1995@gmail.com>
2022-11-29 23:25:53 +0300

    task final commit

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,14 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {
-        client: name + ' ' + phone,
-        order: {
-            address: 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж' + ', кв ' + address.flat,
-            sum: sum
-        },
-        goods: []
-    }
+    let data = {order: {address, sum}, client: {name, phone}, goods: []}
 
     let countOfGoods = goods.length;
 
@@ -49,6 +42,10 @@ function sendRequest(name, phone, address, goods, sum) {
         data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
+    data.order.address = "ул. "`${address.street}` + ", дом " + `${address.house}` + ", " + `${address.entrance}` + ", подъезд, " + `${address.floor}` + " этаж, кв. " + `${address.flat}`;
+    data.order.sum = sum;
+
+    data.client = `${name}` + " " + `${phone}`;
     let jsonData = JSON.stringify({data});
 
     return jsonData;
